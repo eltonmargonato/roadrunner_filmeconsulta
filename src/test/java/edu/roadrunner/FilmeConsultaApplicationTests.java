23f04ce6c69a915a7074f18b4ada0d30ac9f5f12
eltonmargonato <41756807+eltonmargonato@users.noreply.github.com>
2020-02-12 16:59:36 -0300

    Update FilmeConsultaApplicationTests.java

--- src/test/java/edu/roadrunner/FilmeConsultaApplicationTests.java
+++ src/test/java/edu/roadrunner/FilmeConsultaApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ContextConfiguration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
