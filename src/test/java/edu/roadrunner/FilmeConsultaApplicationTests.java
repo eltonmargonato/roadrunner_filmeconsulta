8bbd6a563a2f5d634f0d08a47f98644f66f91d90
eltonmargonato <41756807+eltonmargonato@users.noreply.github.com>
2020-02-12 15:46:51 -0300

    Update FilmeConsultaApplicationTests.java

--- src/test/java/edu/roadrunner/FilmeConsultaApplicationTests.java
+++ src/test/java/edu/roadrunner/FilmeConsultaApplicationTests.java
@@ -1,5 +1,6 @@
 package edu.roadrunner;
 
+import edu.roadrunner.controller.FilmeController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
