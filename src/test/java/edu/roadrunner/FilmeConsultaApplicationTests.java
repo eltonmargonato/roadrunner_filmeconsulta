ab3e9be03e1d0ff076386255f31607a73bb13388
eltonmargonato <41756807+eltonmargonato@users.noreply.github.com>
2020-02-12 16:43:50 -0300

    Update FilmeConsultaApplicationTests.java

--- src/test/java/edu/roadrunner/FilmeConsultaApplicationTests.java
+++ src/test/java/edu/roadrunner/FilmeConsultaApplicationTests.java
@@ -32,8 +32,7 @@ public class FilmeConsultaApplicationTests {
 	@Test
         public void getFilmesPorGenero() throws Exception {
              mockMvc.perform(get("/filme/busca/genero/1").accept(MediaType.APPLICATION_JSON))
-		                      .andExpect(status().isOk())
-                                      .andExpect(MockMvcResultMatchers.jsonPath("$.idFilme").exists());
+		                      .andExpect(status().isOk());
         }
 	
 }
