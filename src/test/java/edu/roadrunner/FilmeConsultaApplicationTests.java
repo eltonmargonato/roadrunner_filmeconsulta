ae388e1326944b2fd31025e2dae93ab80d361be5
eltonmargonato <41756807+eltonmargonato@users.noreply.github.com>
2020-02-12 16:46:14 -0300

    Update FilmeConsultaApplicationTests.java

--- src/test/java/edu/roadrunner/FilmeConsultaApplicationTests.java
+++ src/test/java/edu/roadrunner/FilmeConsultaApplicationTests.java
@@ -11,12 +11,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.http.MediaType;
 
- import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
- import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
- import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
-
-
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @WebMvcTest(FilmeController.class)
