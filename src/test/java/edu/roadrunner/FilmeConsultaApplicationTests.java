525d16b2046f7997b186c5c7ea46f5ad4bd8c77a
eltonmargonato <41756807+eltonmargonato@users.noreply.github.com>
2020-02-12 15:19:28 -0300

    Update FilmeConsultaApplicationTests.java

--- src/test/java/edu/roadrunner/FilmeConsultaApplicationTests.java
+++ src/test/java/edu/roadrunner/FilmeConsultaApplicationTests.java
@@ -7,10 +7,23 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class FilmeConsultaApplicationTests {
+	
+	@Autowired
+        private MockMvc mockMvc;
 
 	@Test
 	public void contextLoads() {
 	}
-
+	
+	@Test
+        public void getFilmePorGenero() throws Exception {
+             mvc.perform( MockMvcRequestBuilders
+                .get("/filme/busca/genero/1")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.idFilme").exists());
+        }
 }
