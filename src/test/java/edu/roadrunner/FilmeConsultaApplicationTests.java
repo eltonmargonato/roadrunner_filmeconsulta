6945760388a1b304f200520ab090d70aaa2b013b
eltonmargonato <41756807+eltonmargonato@users.noreply.github.com>
2020-02-12 17:28:40 -0300

    Update FilmeConsultaApplicationTests.java

--- src/test/java/edu/roadrunner/FilmeConsultaApplicationTests.java
+++ src/test/java/edu/roadrunner/FilmeConsultaApplicationTests.java
@@ -1,18 +1,19 @@
 package edu.roadrunner;
 
-import edu.roadrunner.controller.FilmeController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.ContextConfiguration;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.test.web.servlet.MockMvc;
-
-
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /*
  import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
